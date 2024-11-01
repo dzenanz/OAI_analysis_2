1980d63329374c1f5ebd89ec3419b814e37114f0
Dženan Zukić <dzenan.zukic@kitware.com>
2024-11-01 15:46:52 -0400

    Use the original (non-smooth) atlas mesh
    The 3D to 2D projection results in smoother projection with
    the original atlas mesh, which has staircase-like artifacts.

--- oai_analysis/pipeline.py
+++ oai_analysis/pipeline.py
@@ -125,9 +125,9 @@ def analysis_pipeline(input_path, output_path, laterality, keep_intermediate_out
     atlas_image = itk.imread(atlas_filename, itk.F)
 
     inner_mesh_fc_atlas = mp.read_vtk_mesh(
-        DATA_DIR / "atlases/atlas_60_LEFT_baseline_NMI/atlas_FC_inner_mesh_smooth.ply")
+        DATA_DIR / "atlases/atlas_60_LEFT_baseline_NMI/atlas_FC_inner_mesh_LPS.ply")
     inner_mesh_tc_atlas = mp.read_vtk_mesh(
-        DATA_DIR / "atlases/atlas_60_LEFT_baseline_NMI/atlas_TC_inner_mesh_smooth.ply")
+        DATA_DIR / "atlases/atlas_60_LEFT_baseline_NMI/atlas_TC_inner_mesh_LPS.ply")
 
     print("Registering the input image to the atlas")
     model = get_unigradicon()
